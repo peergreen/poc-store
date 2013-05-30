f301ee2f464bd4c6e5da882b6c8d3e7b8d454722
grace Nefertiti T <grace.tchougbe@peergreen.com>
2013-05-30 15:40:26 +0200

    Renaming EJB session API

---
+++ db-client-api/src/main/java/com/peergreen/store/db/client/ejb/session/api/ISessionCapability.java
@@ -0,0 +1,17 @@
+package com.peergreen.store.db.client.ejb.session.api;
+
+import java.util.Collection;
+
+import com.peergreen.store.db.client.ejb.entity.api.Capability;
+import com.peergreen.store.db.client.ejb.entity.api.Petal;
+
+public interface ISessionCapability {
+
+    Capability addCapability( String namespace, Collection<String> properties);
+    void deleteCapability (int capabilityId);
+    Capability findCapability (int capabilityId);
+    Collection<Petal> collectPetals();
+    Capability addPetal(Petal petal);
+    Capability removePetal (Petal petal);
+
+}
---
+++ db-client-api/src/main/java/com/peergreen/store/db/client/ejb/session/api/ISessionLink.java
@@ -0,0 +1,11 @@
+package com.peergreen.store.db.client.ejb.session.api;
+
+import com.peergreen.store.db.client.ejb.entity.api.Link;
+
+public interface ISessionLink {
+
+    Link addLink(int linkId, String url, String description);
+    void deleteLink(int linkId);
+    Link findLink(int linkId);
+
+}
