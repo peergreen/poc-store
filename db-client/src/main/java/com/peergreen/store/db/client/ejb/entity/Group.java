f49d0d1ce73fd8716eeeea1634515b26024c1f90
grace Nefertiti T <grace.tchougbe@peergreen.com>
2013-05-31 15:08:16 +0200

    Adding methods for the entity group

--- db-client/src/main/java/com/peergreen/store/db/client/ejb/entity/Group.java
+++ db-client/src/main/java/com/peergreen/store/db/client/ejb/entity/Group.java
@@ -17,7 +17,43 @@ public class Group {
 	private String groupname;
 	@ManyToMany(mappedBy="groupSet",cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
 	private List<User> users ;
-	@ManyToMany(mappedBy="groupSet", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
+	@ManyToMany(mappedBy="groupSet",cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
 	private List<Petal> petals ;
+	/**
+	 * @return the groupname
+	 */
+	public String getGroupname() {
+		return groupname;
+	}
+	/**
+	 * @param groupname the groupname to set
+	 */
+	public void setGroupname(String groupname) {
+		this.groupname = groupname;
+	}
+	/**
+	 * @return the users
+	 */
+	public List<User> getUsers() {
+		return users;
+	}
+	/**
+	 * @param users the users to set
+	 */
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+	/**
+	 * @return the petals
+	 */
+	public List<Petal> getPetals() {
+		return petals;
+	}
+	/**
+	 * @param petals the petals to set
+	 */
+	public void setPetals(List<Petal> petals) {
+		this.petals = petals;
+	}
 
 }
