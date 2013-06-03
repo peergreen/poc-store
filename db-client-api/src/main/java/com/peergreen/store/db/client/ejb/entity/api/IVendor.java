db745f974b9d81214577ac3322c4ab2b7c2571cd
grace Nefertiti T <grace.tchougbe@peergreen.com>
2013-06-03 11:19:23 +0200

    Add API for the entity vendor

---
+++ db-client-api/src/main/java/com/peergreen/store/db/client/ejb/entity/api/IVendor.java
@@ -0,0 +1,39 @@
+package com.peergreen.store.db.client.ejb.entity.api;
+
+import java.util.List;
+
+/**
+ * Interface defining a vendor for a petal
+ */
+public interface IVendor {
+
+	/**
+	 * Method to get the description's vendor
+	 * 
+	 * @return the vendorDescription
+	 */
+	String getVendorDescription();
+
+	/**
+	 * Method to set a description for the vendor 
+	 * 
+	 * @param vendorDescription the vendorDescription to set
+	 */
+	void setVendorDescription(String vendorDescription);
+	
+	/**
+	 * Method for retrieve all the petals provided by the vendor instance
+	 * 
+	 * @return List containing petals that are provided by the vendor instance
+	 */
+	List<IPetal> getPetals();
+
+
+	/**
+	 * Method to add petals to the list of petals that are provided by the vendor instance
+	 * 
+	 * @param List containing the petals to set
+	 */
+	void setPetals(List<IPetal> petals);
+
+}
