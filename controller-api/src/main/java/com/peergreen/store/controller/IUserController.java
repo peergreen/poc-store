0c8bb077da5296364f2ff6bb9b67f734abefef46
Guillaume DC <guillaume.dupraz@peergreen.com>
2013-06-04 10:05:54 +0200

    Add documentation

--- controller-api/src/main/java/com/peergreen/store/controller/IUserController.java
+++ controller-api/src/main/java/com/peergreen/store/controller/IUserController.java
@@ -6,6 +6,18 @@ import java.util.Map;
 import com.peergreen.store.db.client.ejb.entity.api.IGroup;
 import com.peergreen.store.db.client.ejb.entity.api.IUser;
 
+/**
+ * Interface defining all user related operations:
+ * <ul>
+ *      <li>retrieve metadata</li>
+ *      <li>retrieve instance</li>
+ *      <li>add instance on database</li>
+ *      <li>remove instance from database</li>
+ *      <li>modify instance on database</li>
+ *      <li>collect all the user's groups</li>
+ * </ul>
+ *
+ */
 public interface IUserController {
     
     /**
--- controller/src/main/java/com/peergreen/store/controller/UserController.java
+++ controller/src/main/java/com/peergreen/store/controller/UserController.java
@@ -24,6 +24,12 @@ public class UserController implements IUserController {
         this.userSession = userSession;
     }
     
+    /**
+     * Method to retrieve user's information.
+     * 
+     * @return indexed collection of user's information or
+     * <em>null</em> if user doesn't exist.
+     */
     @Override
     public Map<String, String> getUserMetadata(String pseudo) {
         IUser user = userSession.findUserByPseudo(pseudo);
@@ -35,26 +41,58 @@ public class UserController implements IUserController {
         return metadata;
     }
 
+    /**
+     * Method to retrieve a user instance from its pseudo.
+     * 
+     * @param pseudo user's pseudo
+     * @return corresponding User instance
+     */
     @Override
     public IUser getUser(String pseudo) {
         return userSession.findUserByPseudo(pseudo);
     }
 
+    /**
+     * Method to add a new user to the database.
+     * 
+     * @param pseudo user's pseudo
+     * @param password user's password
+     * @param email user's email
+     */
     @Override
     public void addUser(String pseudo, String password, String email) {
         userSession.addUser(pseudo, password, email);
     }
 
+    /**
+     * Method to remove a user from the database.
+     * 
+     * @param pseudo user's pseudo
+     */
     @Override
     public void removeUser(String pseudo) {
         userSession.deleteUserbyPseudo(pseudo);
     }
 
+    /**
+     * Method to modify a user account.
+     * 
+     * @param pseudo user's pseudo
+     * @param password user's password
+     * @param email user's email
+     * @return modified user
+     */
     @Override
     public IUser modifyUser(String pseudo, String password, String email) {
         return userSession.updateUser(pseudo, password, email);
     }
 
+    /**
+     * Method to collect all user's groups.
+     * 
+     * @param pseudo user's pseudo
+     * @return list of all user's groups
+     */
     @Override
     public List<IGroup> collectGroups(String pseudo) {
         return (List<IGroup>) userSession.collectGroups(pseudo);
