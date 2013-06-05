17ca5923d188d9226b0252d589fcceefda573db5
grace Nefertiti T <grace.tchougbe@peergreen.com>
2013-06-05 10:30:06 +0200

    Remove unused import

--- db-client-api/src/main/java/com/peergreen/store/db/client/ejb/entity/Capability.java
+++ db-client-api/src/main/java/com/peergreen/store/db/client/ejb/entity/Capability.java
@@ -4,12 +4,9 @@ import java.util.Map;
 import java.util.Set;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
-import javax.persistence.SequenceGenerator;
 
 
 /**
