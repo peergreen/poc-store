366c7ea4c289c67b4cdf8f8a44e66b603bf5ff90
Guillaume DC <guillaume.dupraz@peergreen.com>
2013-06-05 16:40:58 +0200

    Add new methods

--- aether-client-api/src/main/java/com/peergreen/store/aether/client/IPetalsPersistence.java
+++ aether-client-api/src/main/java/com/peergreen/store/aether/client/IPetalsPersistence.java
@@ -8,6 +8,8 @@ import java.io.File;
  * <p>
  * Provides methods to:
  * <ul>
+ *      <li>add a remote repository</li>
+ *      <li>remove a remote repository</li>
  * 		<li>retrieve petal's binary</li>
  *      <li>add a petal to the local repository</li>
  *      <li>retrieve a petal from the local repository</li>
@@ -18,6 +20,20 @@ import java.io.File;
 public interface IPetalsPersistence 
 {
 
+    /**
+     * Method to add a repository to the list of remote repositories.
+     * 
+     * @param url remote repository url
+     */
+    public void addRemoteRepository(String url);
+    
+    /**
+     * Method to remove a repository from the list of remote repositories.
+     * 
+     * @param url remote repository url
+     */
+    public void removeRemoteRepository(String url);
+    
     /**
      * Method to recover petal's binary from its information
      * 
@@ -75,4 +91,14 @@ public interface IPetalsPersistence
      * @return corresponding petal's binary
      */
     File getPetalFromStaging(String vendor, String artifactId, String version);
+    
+    /**
+     * Method to retrieve a petal from all remote repositories
+     * 
+     * @param vendor petal's vendor
+     * @param artifactId petal's artifactId
+     * @param version petal's version
+     * @return corresponding petal's binary
+     */
+    File getPetalFromRemote(String vendor, String artifactId, String version);
 }
--- aether-client/src/main/java/com/peergreen/store/aether/client/impl/DefaultPetalsPersistence.java
+++ aether-client/src/main/java/com/peergreen/store/aether/client/impl/DefaultPetalsPersistence.java
@@ -2,7 +2,20 @@ package com.peergreen.store.aether.client.impl;
 
 import java.io.File;
 
+import org.apache.felix.ipojo.annotations.Requires;
+import org.apache.felix.ipojo.annotations.Validate;
+import org.eclipse.aether.DefaultRepositorySystemSession;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.installation.InstallRequest;
+import org.eclipse.aether.installation.InstallationException;
+import org.eclipse.aether.resolution.ArtifactRequest;
+import org.eclipse.aether.resolution.ArtifactResolutionException;
+import org.eclipse.aether.resolution.ArtifactResult;
+
 import com.peergreen.store.aether.client.IPetalsPersistence;
+import com.peergreen.store.aether.client.util.IAetherUtils;
 
 /**
  * Class to handle petal's persistence relative functionalities.<br />
@@ -18,6 +31,39 @@ import com.peergreen.store.aether.client.IPetalsPersistence;
  */
 public class DefaultPetalsPersistence implements IPetalsPersistence {
 
+    @Requires
+    private IAetherUtils utils;
+    private RepositorySystem system;
+    private DefaultRepositorySystemSession session;
+
+    @Validate
+    private void validate() {
+        system = utils.newRepositorySystem();
+        session = utils.newRepositorySystemSession(system);
+    }
+
+    /**
+     * Method to add a repository to the list of remote repositories.
+     * 
+     * @param url remote repository url
+     */
+    @Override
+    public void addRemoteRepository(String url) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /**
+     * Method to remove a repository from the list of remote repositories.
+     * 
+     * @param url remote repository url
+     */
+    @Override
+    public void removeRemoteRepository(String url) {
+        // TODO Auto-generated method stub
+        
+    }
+    
     /**
      * Method to recover petal's binary from its information
      * 
@@ -28,8 +74,17 @@ public class DefaultPetalsPersistence implements IPetalsPersistence {
      */
     @Override
     public File getPetal(String vendor, String artifactId, String version) {
-        // TODO
-        return null;
+        Artifact artifact = new DefaultArtifact(vendor+":"+artifactId+":"+version);
+        ArtifactRequest artifactRequest = new ArtifactRequest();
+        artifactRequest.setArtifact(artifact);
+        ArtifactResult artifactResult = null;
+        try {
+            artifactResult = system.resolveArtifact(session,artifactRequest);
+        } catch (ArtifactResolutionException e) {
+            e.printStackTrace();
+        }
+        artifact = artifactResult.getArtifact();
+        return artifact.getFile();
     }
 
     /**
@@ -42,8 +97,17 @@ public class DefaultPetalsPersistence implements IPetalsPersistence {
      */
     @Override
     public void addToLocal(String vendor, String artifactId, String version, File petal) {
-        // TODO Auto-generated method stub
-        
+        Artifact jarArtifact = new DefaultArtifact(vendor, artifactId, "", "", version);
+        jarArtifact = jarArtifact.setFile(petal);
+//        Artifact pomArtifact = new SubArtifact( jarArtifact, "", "pom" );
+//        pomArtifact = pomArtifact.setFile( new File( "pom.xml" ) );
+        InstallRequest installRequest = new InstallRequest();
+        installRequest.addArtifact(jarArtifact);
+        try {
+            system.install(session, installRequest);
+        } catch (InstallationException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -71,9 +135,9 @@ public class DefaultPetalsPersistence implements IPetalsPersistence {
     @Override
     public void addToStaging(String vendor, String artifactId, String version, File petal) {
         // TODO Auto-generated method stub
-        
+
     }
-    
+
     /**
      * Method to retrieve a petal from the staging repository
      * 
@@ -84,6 +148,20 @@ public class DefaultPetalsPersistence implements IPetalsPersistence {
      */
     @Override
     public File getPetalFromStaging(String vendor, String artifactId, String version) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * Method to retrieve a petal from all remote repositories
+     * 
+     * @param vendor petal's vendor
+     * @param artifactId petal's artifactId
+     * @param version petal's version
+     * @return corresponding petal's binary
+     */
+    @Override
+    public File getPetalFromRemote(String vendor, String artifactId, String version) {
         // TODO Auto-generated method stub
         return null;
     }
