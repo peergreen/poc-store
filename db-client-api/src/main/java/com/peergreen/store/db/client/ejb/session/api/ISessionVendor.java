fd077d22afe2bf0e516880b2f07845dbedff6b67
grace Nefertiti T <grace.tchougbe@peergreen.com>
2013-06-05 13:52:15 +0200

    Change signature of methods

--- db-client-api/src/main/java/com/peergreen/store/db/client/ejb/session/api/ISessionVendor.java
+++ db-client-api/src/main/java/com/peergreen/store/db/client/ejb/session/api/ISessionVendor.java
@@ -8,11 +8,61 @@ import com.peergreen.store.db.client.ejb.entity.Vendor;
 
 public interface ISessionVendor {
 
+    /**
+     * Method to add a new instance of vendor in the database 
+     * The attribute petals are null when creating the group
+     * 
+     * @param vendorName the vendor's name
+     * @param vendorDescription the vendor's description
+     * 
+     * @return A new instance of Vendor
+     */
 	Vendor addVendor(String vendorName, String vendorDescription);
+	
+	/**
+	 * Method to delete a vendor with the name 'vendorName'
+	 * 
+	 * @param vendorName the name of the vendor to delete
+	 */
 	void deleteVendor(String vendorName);
+	
+	/**
+	 * Method to find the vendor with the name 'vendorName'
+	 * 
+	 * @param vendorName the name of the vendor to find
+	 * 
+	 * @return the vendor with the name 'vendorName'
+	 */
 	Vendor findVendor(String vendorName);
+	
+	/**
+	 * Method to collect all the petals provided by
+	 * the vendor with the name 'vendorName'
+	 * 
+	 * @param vendorName the vendor's name to which we collect petals which he provides
+	 * 
+	 * @return A collection of petals which are provided by the vendor with the name 'vendorName'
+	 */
 	Collection<Petal> collectPetals(String vendorName);
-	Vendor addPetal(Petal petal);
-	Vendor removePetal(Petal petal);
+	
+	/**
+	 * Method to add a new petal to those provided by a vendor
+	 * 
+	 * @param vendor the vendor to which add a new petal to provide
+	 * @param petal the petal to provide by the vendor
+	 * 
+	 * @return A new vendor with the petal added in his list of petals provided
+	 */
+	Vendor addPetal(Vendor vendor, Petal petal);
+	
+	/**
+	 * Method to delete a petal from those provided by a vendor
+     * 
+     * @param vendor the vendor to which remove a petal
+     * @param petal the petal to remove from the list of petals provided by the vendor
+     * 
+     * @return A new vendor with the petal removed from his list of petals provided
+	 */
+	Vendor removePetal(Vendor vendor, Petal petal);
 
 }
--- db-client/src/main/java/com/peergreen/store/db/client/ejb/impl/DefaultVendor.java
+++ db-client/src/main/java/com/peergreen/store/db/client/ejb/impl/DefaultVendor.java
@@ -8,40 +8,41 @@ import com.peergreen.store.db.client.ejb.session.api.ISessionVendor;
 
 public class DefaultVendor implements ISessionVendor {
 
-	@Override
-	public Vendor addVendor(String vendorName, String vendorDescription) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Vendor addVendor(String vendorName, String vendorDescription) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public void deleteVendor(String vendorName) {
-		// TODO Auto-generated method stub
+    @Override
+    public void deleteVendor(String vendorName) {
+        // TODO Auto-generated method stub
+        
+    }
 
-	}
+    @Override
+    public Vendor findVendor(String vendorName) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public Vendor findVendor(String vendorName) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Collection<Petal> collectPetals(String vendorName) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public Collection<Petal> collectPetals(String vendorName) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Vendor addPetal(Vendor vendor, Petal petal) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public Vendor addPetal(Petal petal) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Vendor removePetal(Vendor vendor, Petal petal) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public Vendor removePetal(Petal petal) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
 }
