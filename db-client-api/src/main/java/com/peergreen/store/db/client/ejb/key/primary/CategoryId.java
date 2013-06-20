f7d572edc1d4ee84d75ebd569aebca3eae970121
grace Nefertiti T <grace.tchougbe@peergreen.com>
2013-06-20 15:18:07 +0200

    Class for the primary key of entity Category

---
+++ db-client-api/src/main/java/com/peergreen/store/db/client/ejb/key/primary/CategoryId.java
@@ -0,0 +1,44 @@
+package com.peergreen.store.db.client.ejb.key.primary;
+
+import java.io.Serializable;
+
+public class CategoryId implements Serializable{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -3163373459046602500L;
+
+    private String categoryName;
+
+    private int categoryId;
+
+    /**
+     * @return the categoryName
+     */
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    /**
+     * @param categoryName the categoryName to set
+     */
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    /**
+     * @return the categoryId
+     */
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    /**
+     * @param categoryId the categoryId to set
+     */
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+    
+}
