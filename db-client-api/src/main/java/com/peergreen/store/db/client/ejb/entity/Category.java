5b30e17f2a9934894258f56e5ebe1c016cd464cf
grace Nefertiti T <grace.tchougbe@peergreen.com>
2013-06-20 15:11:58 +0200

    Add new namedquery, constructor with and without field and change the primary key using an idClass

--- db-client-api/src/main/java/com/peergreen/store/db/client/ejb/entity/Category.java
+++ db-client-api/src/main/java/com/peergreen/store/db/client/ejb/entity/Category.java
@@ -1,5 +1,6 @@
 package com.peergreen.store.db.client.ejb.entity;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -8,78 +9,97 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
+
+import com.peergreen.store.db.client.ejb.key.primary.CategoryId;
 
 /**
  * Entity Bean representing the category of a petal  
  */
 @NamedQueries({
     @NamedQuery(
-    name = "Category.findAll",
-    query = "select cat from Category cat")
+            name = "Category.findAll",
+            query = "select cat from Category cat"),
+            @NamedQuery(
+                    name ="CategoryByName",
+                    query ="select cat from Category cat where cat.categoryName = :name")
 })
 @Entity
-@SequenceGenerator(name="idCategorySeq", initialValue=1, allocationSize=50)
+@IdClass(CategoryId.class)
 public class Category{
-    
-	@Id
-	@Column(name ="name")
-	private String categoryName;
-	
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="idCategorySeq")
+
+    @Id
+    @Column(name ="name")
+    private String categoryName;
+
+    @Id
+    @SequenceGenerator(name="idCategorySeq", initialValue=1, allocationSize=50)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="idCategorySeq")
     private int categoryId;
 
-	@OneToMany(mappedBy="category", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
-	private Set<Petal> petals;
+    @OneToMany(mappedBy="category", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
+    private Set<Petal> petals;
 
-	 /**
+
+    public Category() {
+        
+    }
+
+    public Category(String categoryName) {
+        super();
+        this.categoryName = categoryName;
+        Set<Petal> petals = new HashSet<Petal>(); 
+        this.setPetals(petals);
+    }
+
+    /**
      * Method to get Id of a category
      * @return the category's id
      */
     public int getCategoryId() {
         return categoryId;
     }
-	
-	/**
-	 * Method to retrieve the name of the category instance
-	 * 
-	 * @return the category's name
-	 */
-	public String getCategoryName() {
-		return categoryName;
-	}
 
-	/**
-	 * Method to give the name of the category instance 
-	 * 
-	 * @param categoryname the category's name to set
-	 */
-	public void setCategoryName(String categoryname) {
-		this.categoryName = categoryname;
-	}
+    /**
+     * Method to retrieve the name of the category instance
+     * 
+     * @return the category's name
+     */
+    public String getCategoryName() {
+        return categoryName;
+    }
 
-	/**
-	 * Method for retrieve all the petals that belongs to this category
-	 * 
-	 * @return Set containing petals that belongs to this category
-	 */
-	public Set<Petal> getPetals() {
-		return petals;
-	}
+    /**
+     * Method to give the name of the category instance 
+     * 
+     * @param categoryname the category's name to set
+     */
+    public void setCategoryName(String categoryname) {
+        this.categoryName = categoryname;
+    }
 
-	/**
-	 * Method to add petals to the Set of petals that belongs to this category
-	 * 
-	 * @param petals Set containing the petals to set
-	 *//**
-	 * @param petals the petals to set
-	 */
-	public void setPetals(Set<Petal> petals) {
-		this.petals = petals;
-	}
-	
-	
+    /**
+     * Method for retrieve all the petals that belongs to this category
+     * 
+     * @return Set containing petals that belongs to this category
+     */
+    public Set<Petal> getPetals() {
+        return petals;
+    }
+
+    /**
+     * Method to add petals to the Set of petals that belongs to this category
+     * 
+     * @param petals Set containing the petals to set
+     *//**
+     * @param petals the petals to set
+     */
+    public void setPetals(Set<Petal> petals) {
+        this.petals = petals;
+    }
+    
 }
