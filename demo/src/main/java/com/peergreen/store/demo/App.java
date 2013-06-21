429de89781e50acf5023afe8b47012fad45b0df2
Guillaume DC <guillaume.dupraz@peergreen.com>
2013-06-21 15:00:21 +0200

    Integrration tests changes.

--- demo/src/main/java/com/peergreen/store/demo/App.java
+++ demo/src/main/java/com/peergreen/store/demo/App.java
@@ -1,5 +1,7 @@
 package com.peergreen.store.demo;
 
+import java.io.File;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,7 +14,11 @@ import org.apache.felix.ipojo.annotations.Validate;
 import com.peergreen.store.controller.IPetalController;
 import com.peergreen.store.controller.IStoreManagment;
 import com.peergreen.store.db.client.ejb.entity.Capability;
+import com.peergreen.store.db.client.ejb.entity.Category;
+import com.peergreen.store.db.client.ejb.entity.Petal;
 import com.peergreen.store.db.client.ejb.entity.Requirement;
+import com.peergreen.store.db.client.ejb.entity.Vendor;
+import com.peergreen.store.db.client.enumeration.Origin;
 
 @Component
 @Instantiate
@@ -35,17 +41,22 @@ public class App {
 //                "who created JOnAS, the Open Application Server used for" +
 //                "critical production processes");
 //
-        storeManagement.addCategory("Bundle");
+        Category category = storeManagement.addCategory("Bundle");
 
         Set<Capability> capabilities = new HashSet<>();
         Set<Requirement> requirements = new HashSet<>();
         
-//        petalController.addPetal(vendor, artifactId, version, description, category, requirements, capabilities, origin, petalBinary)
+        Vendor vendor = petalController.addVendor("Peergreen",
+                "Peergreen is a software company started by the core team" +
+                "who created JOnAS, the Open Application Server used for" +
+                "critical production processes");
         
-//        petalSession.addPetal(vendor, "Store", "0.1.0-beta", "Store module for Peergreen Platform",
-//                category, capabilities, requirements, Origin.LOCAL);
-//        
-//        Collection<Petal> petalsList = storeManagement.collectPetalsFromLocal();
-//        System.out.println("There are "+petalsList.size()+" petal(s) in local repository");
+        File petalBinary = new File("C:\\Users\\user2\\.m2\\repository\\com\\peergreen\\store\\controller");
+        
+        petalController.addPetal(vendor, "Store", "0.1.0-beta", "Apps Store for Peergreen Platform",
+                category, requirements, capabilities, Origin.LOCAL, petalBinary);
+        
+        Collection<Petal> petalsList = storeManagement.collectPetalsFromLocal();
+        System.out.println("There are "+petalsList.size()+" petal(s) in local repository");
     }
 }
