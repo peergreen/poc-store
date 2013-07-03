72e55bd027de5b8f6daed10abdd6a8b2d90ee519
Guillaume DC <guillaume.dupraz@peergreen.com>
2013-07-03 14:17:10 +0200

    Add UnaryNode

---
+++ ldap-parser-api/src/main/java/com/peergreen/store/ldap/parser/node/UnaryNode.java
@@ -0,0 +1,25 @@
+package com.peergreen.store.ldap.parser.node;
+
+import com.peergreen.tree.Node;
+import com.peergreen.tree.node.SimpleNode;
+
+public class UnaryNode<T> extends SimpleNode<T> {
+
+    private Node<T> child;
+    
+    public UnaryNode(T data) {
+        super(data);
+    }
+
+    public boolean validateNode() {
+        return getChildren().size() == 1;
+    }
+    
+    public Node<T> getChild() {
+        return this.child;
+    }
+    
+    public void setChild(Node<T> child) {
+        this.child = child;
+    }
+}
