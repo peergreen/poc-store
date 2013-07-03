cfd2453e64f001770b24ec455c9aba6c84f1822c
Guillaume DC <guillaume.dupraz@peergreen.com>
2013-07-03 14:17:29 +0200

    Add missing methods.

--- ldap-parser-api/src/main/java/com/peergreen/store/ldap/parser/node/BinaryNode.java
+++ ldap-parser-api/src/main/java/com/peergreen/store/ldap/parser/node/BinaryNode.java
@@ -1,8 +1,12 @@
 package com.peergreen.store.ldap.parser.node;
 
+import com.peergreen.tree.Node;
 import com.peergreen.tree.node.SimpleNode;
 
 public class BinaryNode<T> extends SimpleNode<T> {
+    
+    private Node<T> leftOperand;
+    private Node<T> rightOperand;
     
     public BinaryNode(T data) {
         super(data);
@@ -10,5 +14,21 @@ public class BinaryNode<T> extends SimpleNode<T> {
 
     public boolean validateNode() {
         return getChildren().size() == 2;
+    }
+
+    public Node<T> getLeftOperand() {
+        return leftOperand;
+    }
+
+    public void setLeftOperand(Node<T> leftOperand) {
+        this.leftOperand = leftOperand;
+    }
+
+    public Node<T> getRightOperand() {
+        return rightOperand;
+    }
+
+    public void setRightOperand(Node<T> rightOperand) {
+        this.rightOperand = rightOperand;
     }
 }
--- ldap-parser-api/src/main/java/com/peergreen/store/ldap/parser/node/NaryNode.java
+++ ldap-parser-api/src/main/java/com/peergreen/store/ldap/parser/node/NaryNode.java
@@ -1,9 +1,14 @@
 package com.peergreen.store.ldap.parser.node;
 
+import java.util.List;
+
+import com.peergreen.tree.Node;
 import com.peergreen.tree.node.SimpleNode;
 
 public class NaryNode<T> extends SimpleNode<T> {
 
+    private List<Node<T>> children;
+    
     public NaryNode(T data) {
         super(data);
     }
@@ -11,4 +16,10 @@ public class NaryNode<T> extends SimpleNode<T> {
     public boolean validateNode() {
         return getChildren().size() > 0;
     }
+
+    @Override
+    public List<Node<T>> getChildren() {
+        return this.children;
+    }
+
 }
