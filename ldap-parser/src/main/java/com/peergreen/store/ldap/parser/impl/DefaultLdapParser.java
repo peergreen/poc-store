41dfdec61c44be193c239af0baeb5a6030ba5670
Guillaume DC <guillaume.dupraz@peergreen.com>
2013-07-04 14:37:28 +0200

    Further implementation (not complete).

--- ldap-parser/src/main/java/com/peergreen/store/ldap/parser/impl/DefaultLdapParser.java
+++ ldap-parser/src/main/java/com/peergreen/store/ldap/parser/impl/DefaultLdapParser.java
@@ -1,17 +1,23 @@
 package com.peergreen.store.ldap.parser.impl;
 
 import java.io.PrintStream;
-import java.util.Iterator;
-import java.util.List;
 
 import org.apache.felix.ipojo.annotations.Component;
 import org.apache.felix.ipojo.annotations.Instantiate;
 import org.apache.felix.ipojo.annotations.Provides;
 
-import com.peergreen.store.ldap.parser.Element;
 import com.peergreen.store.ldap.parser.ILdapParser;
 import com.peergreen.store.ldap.parser.InvalidLdapFormatException;
+import com.peergreen.store.ldap.parser.enumeration.BinaryOperators;
+import com.peergreen.store.ldap.parser.enumeration.ComparisonOperators;
+import com.peergreen.store.ldap.parser.enumeration.NaryOperators;
 import com.peergreen.store.ldap.parser.enumeration.Operators;
+import com.peergreen.store.ldap.parser.enumeration.UnaryOperators;
+import com.peergreen.store.ldap.parser.node.BinaryNode;
+import com.peergreen.store.ldap.parser.node.IValidatorNode;
+import com.peergreen.store.ldap.parser.node.NaryNode;
+import com.peergreen.store.ldap.parser.node.OperandNode;
+import com.peergreen.store.ldap.parser.node.UnaryNode;
 import com.peergreen.tree.Node;
 import com.peergreen.tree.node.SimpleNode;
 import com.peergreen.tree.visitor.print.TreePrettyPrintNodeVisitor;
@@ -26,14 +32,10 @@ import com.peergreen.tree.visitor.print.TreePrettyPrintNodeVisitor;
 @Provides
 public class DefaultLdapParser implements ILdapParser {
 
-    private static TreePrettyPrintNodeVisitor<Element> visitor = new TreePrettyPrintNodeVisitor<Element>(System.out) {
+    private static TreePrettyPrintNodeVisitor<String> visitor = new TreePrettyPrintNodeVisitor<String>(System.out) {
         @Override
-        protected void doPrintInfo(PrintStream stream, Node<Element> node) {
-            if (node.getData().isOperator()) {
-                stream.printf("OP %s%n", node.getData().getContent());
-            } else {
-                stream.printf("%s%n", node.getData().getContent());
-            }
+        protected void doPrintInfo(PrintStream stream, Node<String> node) {
+            stream.printf("%s%n", node.getData());
         }
     };
 
@@ -41,10 +43,11 @@ public class DefaultLdapParser implements ILdapParser {
         DefaultLdapParser app = new DefaultLdapParser();
 
 //        String f = "(|(&(a=p)(|(b=p)(c=p)))(&(d=p)(e=p))(|(f=v)(g=x)))";
-        String f = "(~=(groupId=com.peergreen.store)(artifactId=controller)(version=1.6.0))";
+        String f = "(&(groupId<=com.peergreen.store)(artifactId>=controller)(version=1.6.0))";
+//        String f = "(&(!(a<=2)))";
 
         try {
-            Node<Element> res = app.parse(f);
+            Node<String> res = app.parse(f);
             res.walk(visitor);
         } catch (InvalidLdapFormatException e) {
             e.printStackTrace();
@@ -60,11 +63,11 @@ public class DefaultLdapParser implements ILdapParser {
      * @throws InvalidLdapFormatException
      */
     @Override
-    public Node<Element> parse(String filter) throws InvalidLdapFormatException {
+    public Node<String> parse(String filter) throws InvalidLdapFormatException {
         // only check if expression start and finish with parenthesis
         String regex = "^[(].*[)]$";
 
-        Node<Element> root = null;
+        Node<String> root = null;
         
         if (filter.matches(regex)) {
             root = parseNode(filter, 0, null);
@@ -72,7 +75,7 @@ public class DefaultLdapParser implements ILdapParser {
             throw new InvalidLdapFormatException("LDAP expression must begin and finish with parenthesis");
         }
 
-        checkTree(root);
+//        checkTree(root);
 
         return root;
     }
@@ -84,9 +87,8 @@ public class DefaultLdapParser implements ILdapParser {
      * @return tree constructed from LDAP filter parsing.
      * @throws InvalidLdapFormatException
      */
-    protected Node<Element> parseNode(String filter, int position, Node<Element> parentNode) throws InvalidLdapFormatException {
-        Element currentElement;
-        SimpleNode<Element> currentNode = null;
+    protected Node<String> parseNode(String filter, int position, Node<String> parentNode) throws InvalidLdapFormatException {
+        SimpleNode<String> currentNode = null;
         
         if (position < filter.length()) {
             String innerText = getInnerText(filter, position);
@@ -97,12 +99,15 @@ public class DefaultLdapParser implements ILdapParser {
 
                 if (!op.isEmpty()) {
                     // create root node
-                    currentElement = new Element(true, op, 0);
-                    currentNode = new SimpleNode<Element>(currentElement);
+                    IValidatorNode<String> opNode = createOperatorNode(op);
+                    
+                    parseNode(filter, position + op.length() + 1, opNode);
 
-                    parseNode(filter, position + op.length() + 1, currentNode);
-
-                    return currentNode;
+                    if (opNode.validate()) {
+                        return opNode;
+                    } else {
+                        throw new InvalidLdapFormatException();
+                    }
                 } else {
                     throw new InvalidLdapFormatException("First character after initial opening parenthesis must be an operator.");
                     // TODO Can be a leaf too. ex: filter (artifactId=Store)
@@ -123,31 +128,43 @@ public class DefaultLdapParser implements ILdapParser {
                         
                         return parentNode;
                     } else {
-                        String op = readOperator(innerText, 0);
-                        
-                        // operator case
-                        if (!op.isEmpty()) {
-                            // create operator node
-                            Element opElem = new Element(true, op, 0);
-                            SimpleNode<Element> opNode = new SimpleNode<Element>(opElem);
-                            parentNode.getChildren().add(opNode);
-                            opNode.setParent(parentNode);
-                            
-                            // recursive call to parse children
-                            parseNode(filter, position + op.length() + 1, opNode);
-                            
-                            return opNode;
+                        Node<String> n = parseOperatorNode(innerText, parentNode);
+                        if (n != null) {
+                            return n;
                         } else {
-                            // create an "operand" node containing innerText
-                            Element content = new Element(false, innerText, 0);
-                            SimpleNode<Element> node = new SimpleNode<Element>(content);
-                            parentNode.getChildren().add(node);
-                            node.setParent(parentNode);
+                            // create an comparison operator node and operand nodes
+                            String operator = "";
+                            String[] operands = null;
+                            for (ComparisonOperators compOp : ComparisonOperators.values()) {
+                                operands = innerText.split(compOp.getComparisonOperator());
+                                if (operands.length == 2) {
+                                    operator = compOp.getComparisonOperator();
+                                    break;
+                                }
+                            }
+                            
+                            BinaryNode<String> operatorNode = new BinaryNode<String>(operator);
+                            
+                            // create operand nodes
+                            if (!operator.isEmpty()) {
+                                operands = innerText.split(operator);
+                                OperandNode<String> left = new OperandNode<String>(operands[0]);
+                                left.setParent(operatorNode);
+                                OperandNode<String> right = new OperandNode<String>(operands[1]);
+                                right.setParent(operatorNode);
+                            }
+
+                            if (operatorNode.validate()) {
+                                parentNode.getChildren().add(operatorNode);
+                                operatorNode.setParent(parentNode);
+                            } else {
+                                throw new InvalidLdapFormatException("Comparison operator must be applied on two operands.");
+                            }
                             
                             // parse rest of filter
                             parseNode(filter, position + innerText.length() + 2, parentNode);
                             
-                            return node;
+                            return operatorNode;
                         }
                     }
                 } else /* if (innerText.isEmpty()) */ {
@@ -202,6 +219,82 @@ public class DefaultLdapParser implements ILdapParser {
     }
 
     /**
+     * Method to parse a node and its children.
+     * 
+     * @param filter filter to parse
+     * @param parentNode parent node
+     * @return operator node parsed (with its children)
+     * @throws InvalidLdapFormatException
+     */
+    protected IValidatorNode<String> parseOperatorNode(String filter, Node<String> parentNode) throws InvalidLdapFormatException {
+        if (filter.length() > 5) /* minimum length ex: &(x=y) */ {
+            String op = readOperator(filter, 0);
+            
+            // operator case
+            if (!op.isEmpty()) {
+                // create operator node
+                IValidatorNode<String> opNode = createOperatorNode(op);
+                                            
+                // recursive call to parse children
+                parseNode(filter, op.length() + 1, opNode);
+                
+                if (opNode.validate()) {
+                    opNode.setParent(parentNode);
+                    return opNode;
+                } else {
+                    throw new InvalidLdapFormatException("Invalid node.");
+                }
+            } else {
+                return null;
+            }
+        } else {
+            throw new InvalidLdapFormatException("Not enought characters to parse an operator and its operands.");
+        }
+    }
+    
+    protected IValidatorNode<String> parseLeaf(String filter, Node<String> parentNode) throws InvalidLdapFormatException {
+        int position = 0;
+        
+        IValidatorNode<String> n = parseOperatorNode(filter, parentNode);
+        if (n != null) {
+            return n;
+        } else {
+            // create an comparison operator node and operand nodes
+            String operator = "";
+            String[] operands = null;
+            for (ComparisonOperators compOp : ComparisonOperators.values()) {
+                operands = filter.split(compOp.getComparisonOperator());
+                if (operands.length == 2) {
+                    operator = compOp.getComparisonOperator();
+                    break;
+                }
+            }
+            
+            BinaryNode<String> operatorNode = new BinaryNode<String>(operator);
+            
+            // create operand nodes
+            if (!operator.isEmpty()) {
+                operands = filter.split(operator);
+                OperandNode<String> left = new OperandNode<String>(operands[0]);
+                left.setParent(operatorNode);
+                OperandNode<String> right = new OperandNode<String>(operands[1]);
+                right.setParent(operatorNode);
+            }
+
+            if (operatorNode.validate()) {
+                operatorNode.setParent(parentNode);
+            } else {
+                throw new InvalidLdapFormatException("Comparison operator must be applied on two operands.");
+            }
+            
+            // parse rest of filter
+            parseNode(filter, position + filter.length() + 2, parentNode);
+            
+            return operatorNode;
+        }
+    }
+    
+    /**
      * Method to retrieve text between parenthesis.<br />
      * Take all inner parenthesis blocks.<br />
      * For example, getInnerText("(x=((a+b)&c))", 3) will return "(a+b)&c"
@@ -240,6 +333,22 @@ public class DefaultLdapParser implements ILdapParser {
         return innerText;
     }
 
+    protected IValidatorNode<String> createOperatorNode (String op) {
+        IValidatorNode<String> opNode = null;
+        if (UnaryOperators.isUnaryOperator(op)) {
+            opNode = new UnaryNode<String>(op);
+            // TODO add handler
+        } else if (BinaryOperators.isBinaryOperator(op)) {
+            opNode = new BinaryNode<String>(op);
+            // TODO add handler
+        } else if (NaryOperators.isNaryOperator(op)) {
+            opNode = new NaryNode<String>(op);
+            // TODO add handler
+        }
+        
+        return opNode;
+    }
+    
     /**
      * Method to check if tree is valid.<br />
      * Verify every operator node has at least two operands.
@@ -248,18 +357,18 @@ public class DefaultLdapParser implements ILdapParser {
      * @return {@literal true} if tree is valid, {@literal false} otherwise
      * @throws InvalidLdapFormatException
      */
-    protected boolean checkTree(Node<Element> node) throws InvalidLdapFormatException {
-        if (node.getData().isOperator()) {
-            if (node.getData().getOperandsNb() < 2) {
+    protected boolean checkTree(Node<String> node) throws InvalidLdapFormatException {
+//        if (node.getData().isOperator()) {
+//            if (node.getData().getOperandsNb() < 2) {
 //                throw new InvalidLdapFormatException("Operator must be applied on at least two operands");
-            }
-        }
-
-        List<Node<Element>> children = node.getChildren();
-        Iterator<Node<Element>> it = children.iterator();
-        while(it.hasNext()) {
-            checkTree(it.next());
-        }
+//            }
+//        }
+//
+//        List<Node<Element>> children = node.getChildren();
+//        Iterator<Node<Element>> it = children.iterator();
+//        while(it.hasNext()) {
+//            checkTree(it.next());
+//        }
 
         return true;
     }
