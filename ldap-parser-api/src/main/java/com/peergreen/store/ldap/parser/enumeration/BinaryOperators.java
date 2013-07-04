9e5eda0adf38302b7c6d854c54ccfe82bea0b037
Guillaume DC <guillaume.dupraz@peergreen.com>
2013-07-04 14:31:24 +0200

    Add kind of operator enums.

---
+++ ldap-parser-api/src/main/java/com/peergreen/store/ldap/parser/enumeration/BinaryOperators.java
@@ -0,0 +1,23 @@
+package com.peergreen.store.ldap.parser.enumeration;
+
+public enum BinaryOperators {
+    APPROX("~="),
+    LESSER_THAN_EQUAL("<="),
+    GREATER_THAN_EQUAL(">=");
+
+    String s;
+
+    BinaryOperators(String s) {
+        this.s = s;
+    }
+
+    public String getBinaryOperator() {
+        return this.s;
+    }
+    
+    public static boolean isBinaryOperator(String s) {
+        return (s.equals(APPROX.getBinaryOperator()) ||
+                s.equals(GREATER_THAN_EQUAL.getBinaryOperator()) ||
+                s.equals(LESSER_THAN_EQUAL.getBinaryOperator()));
+    }
+}
