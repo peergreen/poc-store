1ad3f96594221e38482f95157fffee571484e73c
Guillaume DC <guillaume.dupraz@peergreen.com>
2013-07-04 17:14:58 +0200

    Add handler management.

---
+++ ldap-parser-api/src/main/java/com/peergreen/store/ldap/parser/handler/ILdapHandler.java
@@ -0,0 +1,16 @@
+package com.peergreen.store.ldap.parser.handler;
+
+import com.peergreen.store.ldap.parser.Element;
+import com.peergreen.tree.Node;
+
+public interface ILdapHandler {
+
+    /**
+     * Method to generate a piece of JPQL query from
+     *  the generated tree using LDAP parser.
+     * 
+     * @param node input tree to transcript on JPQL
+     * @return corresponding piece of JPQL query
+     */
+    String toQueryElement(Node<Element> node);
+}
