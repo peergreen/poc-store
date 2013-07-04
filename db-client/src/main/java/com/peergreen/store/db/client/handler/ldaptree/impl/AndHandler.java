49f3b2ed5c47e042c1eb3c38c49d9f613e8c78b2
grace Nefertiti T <grace.tchougbe@peergreen.com>
2013-07-04 15:13:04 +0200

    Remove bad implementation and implements an other interface

--- db-client/src/main/java/com/peergreen/store/db/client/handler/ldaptree/impl/AndHandler.java
+++ db-client/src/main/java/com/peergreen/store/db/client/handler/ldaptree/impl/AndHandler.java
@@ -1,24 +1,14 @@
 package com.peergreen.store.db.client.handler.ldaptree.impl;
 
-import java.util.List;
-
-import com.peergreen.store.db.client.handler.ldaptree.api.IAndHandler;
+import com.peergreen.store.db.client.handler.ldaptree.api.ILdapHandler;
 import com.peergreen.store.ldap.parser.Element;
 import com.peergreen.tree.Node;
 
-public class AndHandler implements IAndHandler {
+public class AndHandler implements ILdapHandler {
 
     @Override
     public String toQueryElement(Node<Element> node) {
-        List<Node<Element>> children = node.getChildren();
-
-        Node<Element> childL = children.get(0);
-        Node<Element> childR = children.get(1);
-
-        String childLString = "c." + childL.getData().getContent();
-        String childRString = '\'' + childR.getData().getContent() + '\'';
-
-        return childLString.concat("" + "and " +childRString);
+       return null;
     }
 
 }
