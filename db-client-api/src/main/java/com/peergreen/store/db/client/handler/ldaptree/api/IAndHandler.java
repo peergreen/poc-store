7571a6f0c2ab64d1bb919b2aeb96ca3e464242be
Guillaume DC <guillaume.dupraz@peergreen.com>
2013-07-18 14:53:34 +0200

    Remove unused classes.

--- db-client-api/src/main/java/com/peergreen/store/db/client/handler/ldaptree/api/IAndHandler.java
+++
@@ -1,7 +0,0 @@
-package com.peergreen.store.db.client.handler.ldaptree.api;
-
-import com.peergreen.tree.Node;
-
-public interface IAndHandler {
-    String toQueryElement(Node<String> node);
-}
--- db-client-api/src/main/java/com/peergreen/store/db/client/handler/ldaptree/api/IOrHandler.java
+++
@@ -1,7 +0,0 @@
-package com.peergreen.store.db.client.handler.ldaptree.api;
-
-import com.peergreen.tree.Node;
-
-public interface IOrHandler {
-    String toQueryElement(Node<String> node);
-}
