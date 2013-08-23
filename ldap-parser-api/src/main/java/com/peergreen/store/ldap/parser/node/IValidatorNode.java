55ffff7c848be281b05235a3714557f15aeec5e0
Guillaume DC <guillaume.dupraz@peergreen.com>
2013-08-23 10:59:44 +0200

    Remove setters (not exposed in public interface). Handler is no longer used.

--- ldap-parser-api/src/main/java/com/peergreen/store/ldap/parser/node/IValidatorNode.java
+++ ldap-parser-api/src/main/java/com/peergreen/store/ldap/parser/node/IValidatorNode.java
@@ -3,12 +3,11 @@ package com.peergreen.store.ldap.parser.node;
 import java.util.Collection;
 
 import com.peergreen.store.ldap.parser.exception.InvalidLdapFormatException;
-import com.peergreen.store.ldap.parser.handler.ILdapHandler;
 import com.peergreen.tree.Node;
 
 
 public interface IValidatorNode<T> extends Node<T> {
-    
+
     /**
      * Method to validate the node.<br />
      * Mainly check cardinality consistency.<br />
@@ -18,28 +17,21 @@ public interface IValidatorNode<T> extends Node<T> {
      * @throws InvalidLdapFormatException
      */
     boolean validate() throws InvalidLdapFormatException;
-    
+
     /**
      * Method to retrieve parent node, using IValidatorNode<T> format.
      * 
      * @return parent node
      */
     IValidatorNode<T> getParentValidatorNode();
-    
-    /**
-     * Method to set parent node, using IValidatorNode<T> format.
-     * 
-     * @param parentNode parent node to set
-     */
-    void setParentValidatorNode(IValidatorNode<T> parentNode);
-    
+
     /**
      * Method to get children nodes, using IValidatorNode<T> format.
      * 
      * @return list of children nodes, using IValidatorNode<T> format.
      */
-    Collection<IValidatorNode<T>> getChildrenValidatorNode();
-    
+    Collection<? extends IValidatorNode<T>> getChildrenValidatorNode();
+
     /**
      * Method to add a new children to current node, using IValidatorNode<T> format.<br />
      * Throws {@link InvalidLdapFormatException} if operation does not respect node validity.
@@ -48,29 +40,7 @@ public interface IValidatorNode<T> extends Node<T> {
      * @throws InvalidLdapFormatException
      */
     void addChildValidatorNode(IValidatorNode<T> child) throws InvalidLdapFormatException;
-    
-    /**
-     * Method to retrieve associated handler.
-     * 
-     * @return associated handler
-     */
-    ILdapHandler getHandler();
-    
-    /**
-     * Method to set associated handler.
-     * 
-     * @param handler associated hendler to set
-     */
-    void setHandler(ILdapHandler handler);
-    
-    /**
-     * Method to set (or replace) a property.
-     * 
-     * @param propClass property class
-     * @param property  property
-     */
-    <Prop> void setProperty(Class<Prop> propClass, Prop property);
-    
+
     /**
      * Method to retrieve a property.
      * 
@@ -78,4 +48,5 @@ public interface IValidatorNode<T> extends Node<T> {
      * @return property
      */
     <Prop> Prop getProperty(Class<Prop> propClass);
+
 }
