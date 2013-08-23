b3577a265811fec51cabcf7661a6100c99510bc1
Guillaume DC <guillaume.dupraz@peergreen.com>
2013-08-23 11:20:42 +0200

    Fix types.

--- ldap-parser-api/src/main/java/com/peergreen/store/ldap/parser/handler/ILdapHandler.java
+++ ldap-parser-api/src/main/java/com/peergreen/store/ldap/parser/handler/ILdapHandler.java
@@ -1,6 +1,10 @@
 package com.peergreen.store.ldap.parser.handler;
 
 import com.peergreen.store.ldap.parser.INodeContext;
+import com.peergreen.store.ldap.parser.node.IBinaryNode;
+import com.peergreen.store.ldap.parser.node.INaryNode;
+import com.peergreen.store.ldap.parser.node.IUnaryNode;
+import com.peergreen.store.ldap.parser.node.IValidatorNode;
 
 
 /**
@@ -11,28 +15,30 @@ import com.peergreen.store.ldap.parser.INodeContext;
 public interface ILdapHandler {
 
     /**
-     * Method to generate query for node.
+     * Method called when all node children have been created.
+     * 
+     * @param nodeContext node context
      */
-    void toQueryElement();
-    
+    void afterCreatingAllChildren(INodeContext<? extends IValidatorNode<String>> nodeContext);
+
     /**
      * Method called when an unary node is created.
      * 
-     * @param created unary node
+     * @param nodeContext node context
      */
-    void onUnaryNodeCreation(INodeContext<String> nodeContext);
-    
+    void onUnaryNodeCreation(INodeContext<? extends IUnaryNode> nodeContext);
+
     /**
      * Method called when a binary node is created.
      * 
-     * @param created binary node
+     * @param nodeContext node context
      */
-    void onBinaryNodeCreation(INodeContext<String> nodeContext);
-    
+    void onBinaryNodeCreation(INodeContext<? extends IBinaryNode> nodeContext);
+
     /**
      * Method called when an n-ary node is created.
      * 
-     * @param created n-ary node
+     * @param nodeContext node context
      */
-    void onNaryNodeCreation(INodeContext<String> nodeContext);
+    void onNaryNodeCreation(INodeContext<? extends INaryNode> nodeContext);
 }
