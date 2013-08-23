724aa43fce30cd9116d2d88a298ce9f71f86ee9b
Guillaume DC <guillaume.dupraz@peergreen.com>
2013-08-23 11:20:22 +0200

    Add needed methods to generate query from node (using JpaContext content).

--- db-client/src/main/java/com/peergreen/store/db/client/ldap/handler/client/jpql/impl/JpaContext.java
+++ db-client/src/main/java/com/peergreen/store/db/client/ldap/handler/client/jpql/impl/JpaContext.java
@@ -1,25 +1,80 @@
 package com.peergreen.store.db.client.ldap.handler.client.jpql.impl;
 
+import java.util.Collection;
+
 import javax.persistence.criteria.AbstractQuery;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Subquery;
+
+import com.peergreen.store.db.client.ejb.entity.Capability;
+import com.peergreen.store.ldap.parser.INodeContext;
+import com.peergreen.store.ldap.parser.node.IValidatorNode;
 
 
+/**
+ * @author user2
+ *
+ * @param <T>
+ */
 public class JpaContext<T> {
-    private AbstractQuery<T> parentQuery;
-    private AbstractQuery<T> generatedQuery;
+    private boolean negated;
+    private AbstractQuery<T> subquery;
+    private INodeContext<? extends IValidatorNode<String>> nodeContext;
+    private IQueryGenerator handler;
     
+    public void negate() {
+        negated = !negated;
+        Collection<? extends IValidatorNode<String>> children = nodeContext.getNode().getChildrenValidatorNode();
+        for (IValidatorNode<String> c : children) {
+            c.getProperty(JpaContext.class).negate();
+        }
+    }
+    
+    @SuppressWarnings("unchecked")
     public AbstractQuery<T> getParentQuery() {
-        return parentQuery;
+        if (nodeContext.getProperty(Boolean.class)) {
+            // retrieve main query (CriteriaQuery)
+            return nodeContext.getParser().getProperty(CriteriaQuery.class);
+        } else {
+            // retrieve parent subquery
+            return nodeContext.getNode().getParentValidatorNode().getProperty(JpaContext.class).getSubquery();
+        }
     }
     
-    public void setParentQuery(AbstractQuery<T> parentQuery) {
-        this.parentQuery = parentQuery;
+    public AbstractQuery<T> getSubquery() {
+        return this.subquery;
     }
     
-    public AbstractQuery<T> getGeneratedQuery() {
-        return generatedQuery;
+    public void setSubquery(AbstractQuery<T> subquery) {
+        this.subquery = subquery;
     }
     
-    public void setGeneratedQuery(AbstractQuery<T> generatedQuery) {
-        this.generatedQuery = generatedQuery;
+    public Subquery<Capability> getGeneratedQuery() {
+        return handler.getQuery(nodeContext, negated);
     }
+    
+    public boolean isNegated() {
+        return negated;
+    }
+
+    public void setNegated(boolean negated) {
+        this.negated = negated;
+    }
+
+    public INodeContext<? extends IValidatorNode<String>> getNodeContext() {
+        return nodeContext;
+    }
+
+    public void setNodeContext(INodeContext<? extends IValidatorNode<String>> nodeContext) {
+        this.nodeContext = nodeContext;
+    }
+
+    public IQueryGenerator getHandler() {
+        return handler;
+    }
+
+    public void setHandler(IQueryGenerator handler) {
+        this.handler = handler;
+    }
+    
 }
