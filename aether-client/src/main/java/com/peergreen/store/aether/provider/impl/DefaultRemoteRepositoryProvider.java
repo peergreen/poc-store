ab7fb3a96668760e03bfe45ba37ed7775bfc52df
Guillaume DC <guillaume.dupraz@peergreen.com>
2013-09-02 12:01:08 +0200

    Revert "Write REST Client to retrieve petal from remote."
    This reverts commit d40accf45823b94a6f2a0a5f531585876afce382.

--- aether-client/src/main/java/com/peergreen/store/aether/provider/impl/DefaultRemoteRepositoryProvider.java
+++ aether-client/src/main/java/com/peergreen/store/aether/provider/impl/DefaultRemoteRepositoryProvider.java
@@ -1,49 +1,32 @@
 package com.peergreen.store.aether.provider.impl;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import org.apache.felix.ipojo.annotations.Component;
 import org.apache.felix.ipojo.annotations.Property;
 import org.apache.felix.ipojo.annotations.Provides;
 import org.apache.felix.ipojo.annotations.ServiceProperty;
 import org.apache.felix.ipojo.annotations.Validate;
+import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.repository.RemoteRepository;
-
+import org.eclipse.aether.resolution.ArtifactRequest;
+import org.eclipse.aether.resolution.ArtifactResolutionException;
+import org.eclipse.aether.resolution.ArtifactResult;
 
 /**
  * Class defining methods for remote repository provider.
  */
 @Component
 @Provides
-public class DefaultRemoteRepositoryProvider extends
-DefaultRepositoryProvider<RemoteRepository> {
+public class DefaultRemoteRepositoryProvider extends DefaultRepositoryProvider<RemoteRepository> {
 
+    private RemoteRepository repository;
     // retrieved from Config Admin
     @ServiceProperty
     private String name;
     @Property
     private String path;
-    @Property
-    private String tmpPath;
-
-    public DefaultRemoteRepositoryProvider() {
-        
-    }
-    
-    public DefaultRemoteRepositoryProvider(String url) {
-        this.path = url;
-    }
     
     /**
      * Method to build up requirements to use Aether with remote repository.
@@ -51,9 +34,10 @@ DefaultRepositoryProvider<RemoteRepository> {
     @Validate
     @Override
     public void init() {
-
+        validate();
+        repository = new RemoteRepository.Builder(name, "default", path).build();
     }
-
+    
     /**
      * Method to retrieve a petal's binary from the repository.
      * 
@@ -63,53 +47,26 @@ DefaultRepositoryProvider<RemoteRepository> {
      * @return
      */
     @Override
-    public File retrievePetal(String vendor,
-            String artifactId,
-            String version) {
-        // TODO
-        ClassLoader classloaderOld = Thread.currentThread().getContextClassLoader();
-        Thread.currentThread().setContextClassLoader(DefaultRemoteRepositoryProvider.class.getClassLoader());
+    public File retrievePetal(String vendor, String artifactId, String version) {
+        Artifact artifact = new DefaultArtifact(vendor+":"+artifactId+":"+version);
+        ArtifactRequest artifactRequest = new ArtifactRequest();
+        artifactRequest.addRepository(repository);
+        artifactRequest.setArtifact(artifact);
+        ArtifactResult artifactResult = null;
         
+        // better to throw exception and manage it on call level
         try {
-            Client client = ClientBuilder.newClient();
-    
-            // entity getContent => il devrait y avoir un inputStream
-            Response response = client.target(path + "/petal/local/" + vendor +
-                    "/" + artifactId + "/" + version)
-                    .request(MediaType.MULTIPART_FORM_DATA).get();
-    
-            BufferedInputStream bis = new BufferedInputStream(
-                    (InputStream)response.getEntity());
-    
-            // TODO change to dynamic name
-            File tmpFile = new File(tmpPath + "/test.jar");
-    
-            BufferedOutputStream bos = null;
-            try {
-                bos = new BufferedOutputStream(new FileOutputStream(tmpFile));
-            } catch (FileNotFoundException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-    
-            int bytesRead = 0;
-            byte[] buffer = new byte[1024];
-    
-            // Keep reading until there is no more content left.
-            // -1 (EOF) is returned when end of file is reached.
-            try {
-                while ((bytesRead = bis.read(buffer)) != -1) {
-                    bos.write(buffer);
-                }
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            
-            return tmpFile;
-        } finally {
-            Thread.currentThread().setContextClassLoader(classloaderOld);
+            artifactResult = getSystem().resolveArtifact(getSession(),artifactRequest);
+        } catch (ArtifactResolutionException e) {
+            e.printStackTrace();
         }
+        artifact = artifactResult.getArtifact();
+        
+        return artifact.getFile();
+    }
+
+    public void setRepository(RemoteRepository repository) {
+        this.repository = repository;
     }
 
     public void setName(String name) {
@@ -119,9 +76,9 @@ DefaultRepositoryProvider<RemoteRepository> {
     public String getPath() {
         return path;
     }
-
+    
     public void setPath(String path) {
         this.path = path;
     }
-
+    
 }
